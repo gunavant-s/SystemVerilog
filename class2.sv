18415641a49bdcf3e3170488ae74f0ef8b03f65b
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-09-29 06:21:28 +0530

    Create class2.sv

---
+++ class2.sv
@@ -0,0 +1,37 @@
+class first;
+  
+  int data = 34;
+  
+  task display();
+    $display("Value of data : %0d",data);
+  endtask 
+  
+endclass
+ 
+class second;
+  
+  first f1;
+  
+  function new();
+    f1 = new();
+  endfunction
+   
+endclass
+ 
+ 
+module tb;
+  
+  second s;
+  
+  initial begin
+    s = new();
+    $display("Value of Data : %0d",s.f1.data);
+    s.f1.display();
+    
+    s.f1.data = 45;
+    s.f1.display();
+    
+    
+  end
+
+endmodule
