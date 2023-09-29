fdfcbfa2741258f2d6474511d8be9193684b755f
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-09-29 06:19:05 +0530

    Update and rename this_keyword_in_constructor.sv to function_in_constructor_ex.sv

--- function_in_constructor_ex.sv
+++ function_in_constructor_ex.sv
@@ -1,10 +1,15 @@
+// Assume the class consists of three 8-bit data members a, b, and c. 
+// Create a Custom Constructor that allows the user to update the value of these data members while adding a constructor to the class. 
+// Test your code by adding the value of 2, 4, and 56 to a, b and c respectively.
+
+
 class first;
   
   int data1;
   bit [7:0] data2;
   shortint data3;
   
-  function new(input int data1 = 0, input bit[7:0] data2 = 8'h00, input 		 shortint data3 = 0);
+  function new(input int data1 = 0, input bit[7:0] data2 = 8'h00, input shortint data3 = 0);
      this.data1 = data1;
      this.data2 = data2;
      this.data3 = data3;    
