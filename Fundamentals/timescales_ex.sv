370f12f5719e2c12dcdc7c9a3473923db2db605b
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-02 13:10:00 +0530

    Timescale for precision 3

--- Fundamentals/timescales_ex.sv
+++ Fundamentals/timescales_ex.sv
@@ -2,31 +2,50 @@
 // 1ns / 1ns = 1 = 10^0. No digits after decimal
 // 1ns / 1ps = 10^3. Upto 3 digits after decimal
 
-`timescale 1ns / 1ps   //10^3 -> 3
+// `timescale 1ns / 1ps   //10^3 -> 3
  
+// module tb();
+//   reg clk16 = 0;
+//   reg clk8 = 0;  ///initialize variable
+  
+ 
+//    always #31.25 clk16 = ~clk16;
+//    always #62.5 clk8 = ~clk8;
+  
+ 
+ 
+//   initial begin
+//     $dumpfile("dump.vcd");
+//     $dumpvars;
+//   end
+ 
+ 
+//   initial begin
+//     #200;
+//     $finish();
+//   end
+  
+// endmodule
+
+
+//Write a code to generate a 9MHz square waveform for the signal sclk. Assume timescale with 1nsec time-unit and 3 digit precision.
+
+`timescale 1ns/1ps
+
 module tb();
+  reg clk = 0;
+
+  always #55.555 clk = ~clk;
  
-  
- 
-  
-  reg clk16 = 0;
-  reg clk8 = 0;  ///initialize variable
-  
- 
-   always #31.25 clk16 = ~clk16;
-   always #62.5 clk8 = ~clk8;
-  
- 
+  initial begin
+    #2000
+    $finish;
+  end
  
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars;
   end
- 
- 
-  initial begin
-    #200;
-    $finish();
-  end
-  
 endmodule
+
+
