eed365662e29752849e7574c6ce3d08f7ff259c7
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-04 16:14:28 +0530

    Array operation - Copy

--- Fundamentals/array_copy.sv
+++ Fundamentals/array_copy.sv
@@ -9,3 +9,15 @@ module tb;
     $display("Array2 : %0p",arr2); //Array2 : 95 95 95 95 95 95 95 95 95 95
   end
 endmodule
+
+//Another case when both not same size
+module tb;
+  int arr1[10] = '{10{95}};
+	int arr2[11]; //Since 2-state datatype default value is 0
+	
+  initial begin
+    arr2[0:9] = arr1;
+	  $display("Array1 : %0p",arr1); //Array1 : 95 95 95 95 95 95 95 95 95 95
+	  $display("Array2 : %0p",arr2); //Array2 : 95 95 95 95 95 95 95 95 95 95 0
+  end
+endmodule
