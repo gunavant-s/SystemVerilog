7e3ca66ad90871f26338b59aad40485a0eceb673
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-05 08:30:51 +0530

    User-defined constructor "new"

--- 2-user_defined_constructor.sv
+++ 2-user_defined_constructor.sv
@@ -1,20 +1,19 @@
+//The constructor can be used for initializing the class properties. By creating function in class with name "new".
+
 class first;
+  int data; // first we have to declare the variable
   
-  int data;
-  
-  function new(input int datain = 0);
-    data = datain;
+  function new(input int datain = 0); //default value is given as 0
+    data = datain; //no return type required
   endfunction
-  
 endclass
 
 module tb;
+  first f;//handler
   
-  first f;
   initial begin
-    f = new(45);
+    f = new(45);//initializing
     $display("%0d",f.data);
   end
-  
 endmodule
     
