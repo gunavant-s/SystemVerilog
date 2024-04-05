f152bf784a69d381aac6586475e269c7e416a2e3
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-05 16:27:30 +0530

    Constructor with usage of this keyword

--- 3-custom_constructor.sv
+++ 3-custom_constructor.sv
@@ -1,7 +1,7 @@
 class first;
   bit[7:0] a,b,c;
   
-  function new(input bit[7:0] a,b,c);
+  function new(input bit[7:0] a,b = 90,c);
     this.a = a; // Which a,b,c should get assigned ?
     this.b = b; //a,b,c variable in first class should be assigned with local variable a,b,c in new()
     this.c = c;
@@ -12,7 +12,8 @@ module tb;
   first f;
   
   initial begin
-    f = new(2,4,56);
+    f = new(2, 4, 56);
+    // f = new(2, , 56); This can also be used. Dont mess with order
     $display("a, b, c : %0d, %0d, %0d",f.a,f.b,f.c);
   end
 endmodule
