334ab080223df5adb6662d292ac012cd268578df
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-05 16:53:12 +0530

    Copying an object of class

--- copy_obj.sv
+++ copy_obj.sv
@@ -1,37 +1,27 @@
 class first;
-  
   int data;
+  int data2;
+  
+  function new(input int datain=90);
+    data = datain;
+  endfunction
   
 endclass
  
 module tb;
-  
   first f1;
-  first p1;
-  
+  first f2;
   
   initial begin
-    f1 = new();  ///1. constructor 
-    ///////////////
-    f1.data = 24;   ///2. processing
-    /////////////////
+    f1 = new(88);  ///1. constructor 
+    f1.data2 = 24;   ///2. processing
+  
+    f2 = new f1;  /// 3. copying data from f1 to p1
     
-    p1 = new f1;  /// 3. copying data from f1 to p1
+    $display("Value of data member f2 : %0d", f2.data);  //4. processing 
     
-    /////////////
+    f2.data = 12; // data in f1 won't change
     
-    $display("Value of data member : %0d", p1.data);  //4. processing 
-    
-    p1.data = 12; //
-    
-    $display("Value of data member : %0d", f1.data);  //4. processing 
-
-    // # KERNEL: Value of data member : 24
-    // # KERNEL: Value of data member : 24
-       
-    
+    $display("Value of data member f1 : %0d", f1.data);  //4. processing     
   end
-  
-  
-  
 endmodule
