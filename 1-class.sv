218a60bd77ae5f56325e69ff2032a235a73964b7
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-05 08:26:58 +0530

    Creating class

--- 1-class.sv
+++ 1-class.sv
@@ -1,15 +1,10 @@
 class first;
-  
   reg [5:0] data1; 
   reg [6:0] data2;
   reg [6:0] data3;
-  
-  
 endclass
  
- 
-module tb;
-  
+module tb; 
   first f; //handler
   
   initial begin
@@ -20,5 +15,4 @@ module tb;
     f.data3 = 7'd90;
     $display("%0p %0p %0p",f.data1,f.data2,f.data3); //accessing of values of f
   end
-  
 endmodule
