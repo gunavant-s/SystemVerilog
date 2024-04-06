0c87e46f2059b6bbffde919ff07f9bba813ceef1
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-06 08:30:31 +0530

    modify functionality of base class

--- Inheritance-and-Polymorphism/polymorphism.sv
+++ Inheritance-and-Polymorphism/polymorphism.sv
@@ -2,35 +2,27 @@
 // to execute methon in extended class use define method in base class as "virutal" method
 
 class first;  ///parent class
-  
   int data = 12;
   
-  virtual function void display();
+  virtual function void display(); //member function that you expect to be redefined in derived classes
      $display("FIRST : Value of data : %0d", data);
   endfunction
-   
 endclass
  
- 
-class second extends first; //child class
-  
+class second extends first; //child class 
   int temp = 34;
   
   function void add();
     $display("secomd:Value after process : %0d", temp+4);
   endfunction
-  
  
   function void display();
     $display("SECOND : Value of data : %0d", data);
   endfunction
   //if no function is present in extended class then the base class method executes
-  
 endclass
  
- 
-module tb;
-  
+module tb; 
   first f;
   second s;
   
@@ -41,5 +33,4 @@ module tb;
     f = s;
     f.display();  
   end
-     
 endmodule
