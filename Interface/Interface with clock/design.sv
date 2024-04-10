4d17770bb5a9df37528a7acc7f2bdc8fa3b8ad2e
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-10 16:06:34 +0530

    Using output reg

--- Interface/Interface with clock/design.sv
+++ Interface/Interface with clock/design.sv
@@ -1,7 +1,7 @@
 module add(input [3:0] a,
            input [3:0] b,
            input clk,
-           output reg [4:0] sum
+           output reg [4:0] sum // Reg is used as here we made use of "always" block else internal problem occurs
           );
   
   always @(posedge clk) begin
