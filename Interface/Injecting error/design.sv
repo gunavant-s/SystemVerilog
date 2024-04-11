1b22daec00727884b2dc9d0eb0bc8441d0899358
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-11 11:32:59 +0530

    Design

---
+++ Interface/Injecting error/design.sv
@@ -0,0 +1,11 @@
+// Code your design here
+module add(input [3:0] a,
+           input [3:0] b,
+           output reg [4:0] sum, // Reg is used as here we made use of "always" block else internal problem occurs
+           input clk
+          );
+  
+  always @(posedge clk) begin
+    sum <= a + b;
+  end
+endmodule
