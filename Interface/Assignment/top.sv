2f7b629cc7a7e96116574285159b4d9fd768f214
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-11 08:07:27 +0530

    Design

---
+++ Interface/Assignment/top.sv
@@ -0,0 +1,15 @@
+// Code your design here
+module top
+(
+  input clk,
+  input [3:0] a,b,
+  output reg [7:0] mul
+);
+  
+  always@(posedge clk)
+    begin
+     mul <= a * b;
+    end
+  
+endmodule
+
