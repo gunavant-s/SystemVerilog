d28c295670509b5de87a1f319ea3d6804b4ee9b9
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-04-14 11:12:46 +0530

    Always block example

--- Fundamentals/always_block.sv
+++ Fundamentals/always_block.sv
@@ -1,6 +1,11 @@
 // Assume `timescale 1ps/1ps. Generate a 25 MHz square wave waveform for the Signal clk.
 // Half time period = (1 / (25MHz))/2 = 20ns
 
+// In verilog we have used always @(*) to code combinational logic. 
+// SystemVerilog defines four forms of always procedures: always, always_comb, always_ff, always_latch. 
+// Refer : https://www.verilogpro.com/systemverilog-always_comb-always_ff/
+
+
 `timescale 1ps/1ps
 
 module ex();
@@ -8,5 +13,17 @@ module ex();
   
   initial clk = 1'b0;
   always #20 clk = ~clk;
+
+  /*
+  The always block triggers whenever there is change in variable specified
+  always @ (input1) begin
+    case (input1)
+      1 : output1 = 1;
+      2 : output1 = 2;
+      .......
+    endcase
+  end
+
+  */
   #500 $finish;
 endmodule
