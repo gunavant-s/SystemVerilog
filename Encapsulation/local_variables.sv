6606366c1b76c1e288b1832f9e7e9934c2a8fab3
Gunavant-S <139606020+gunavant-s@users.noreply.github.com>
2025-03-02 15:45:58 -0500

    Using local bit variable

---
+++ Encapsulation/local_variables.sv
@@ -0,0 +1,52 @@
+class Packet;
+  local bit [31:0] data;
+
+  // Constructor
+  function new(bit [31:0] data);
+    this.data = data;  
+  endfunction
+
+  // Declare external methods and tasks
+  // prototypes
+  extern function void display();
+  extern task process();
+  extern task minus();
+
+// Implementation of the display method
+function void Packet::display();
+  $display("Packet data: %0h", data);
+endfunction
+
+// Implementation of the minus task
+task Packet::minus();
+  $display("The packed data - 1 : %0h", data - 1);
+endtask
+
+// Implementation of the process task
+task Packet::process();
+  #10 data = data + 1;
+  $display("Processed data: %0h", data);
+endtask
+
+// Testbench module
+module test;
+  // Declare clock signal
+  bit clk;
+
+  // Generate clock signal
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk; // Toggle clock every 5 time units
+  end
+
+  initial begin
+    Packet pkt = new(32'hABCD1234); // Allocate a new object
+
+    pkt.display();   // Call display method
+    pkt.process();   // Call process task
+    pkt.minus();     // Call minus task
+
+    pkt = null;      // Deallocate the object
+    $finish;         // End simulation
+  end
+endmodule
